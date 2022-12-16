82393daf3e272a48ccbf13bc83d50e04afd36fa5
Brian Craig <briancraigok@gmail.com>
2022-12-16 03:04:05 -0300

    feat: add shader decoration

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,31 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:shader_toy/shader_decoration.dart';
 
 part 'main.g.dart';
 
 late DateTime now;
 
-void main() {
+late FragmentProgram fp;
+
+void main() async {
   now = DateTime.now();
+  fp = await FragmentProgram.fromAsset('assets/flutter-shaders/red-original-lol.glsl');
   runApp(const FlutterApp());
 }
+
+const bigText = '''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In a neque mi. Nam ornare fringilla lorem, sed condimentum lorem gravida quis. Ut non mollis nunc, quis lobortis est. Aliquam vel rutrum felis, vel blandit diam. Duis maximus nisl elit. Mauris blandit ac tortor a hendrerit. Praesent pretium ligula non risus tincidunt, sed luctus nunc condimentum. Sed vel cursus leo. Phasellus euismod enim ut nisl semper, a facilisis justo ultrices. Mauris at iaculis tellus, at dignissim risus. Morbi neque justo, viverra eget ipsum sed, bibendum consectetur libero. Vestibulum aliquam fringilla auctor. Vivamus quam neque, iaculis at nunc vel, gravida scelerisque sem. Etiam efficitur luctus eros, volutpat sodales massa varius sed. Pellentesque nec vehicula ipsum. Maecenas consectetur tellus sit amet quam tempus cursus.
+
+Donec finibus lectus non eros condimentum, semper consequat ex interdum. Duis eget diam eget tellus porta dictum. Fusce sodales pretium odio ac vehicula. Phasellus rhoncus sapien in fringilla mattis. Donec elit quam, luctus vel lorem id, dictum rutrum velit. Aenean eleifend ut sapien et porttitor. Phasellus ipsum ex, viverra ut orci eu, vestibulum tincidunt ex. Etiam est turpis, fermentum at venenatis et, commodo eget lectus. Donec mattis suscipit magna, in vehicula justo molestie eu.
+
+Donec eu dolor posuere, convallis ex vitae, feugiat nunc. Aenean augue lacus, sollicitudin vitae ligula ac, consectetur ornare urna. Aliquam erat volutpat. Nam sapien quam, condimentum ac ligula at, elementum accumsan sem. Duis vitae dui non nisi lacinia lacinia congue quis purus. Pellentesque dictum ipsum vel ullamcorper dapibus. Etiam pretium bibendum sagittis.
+
+Nullam gravida tincidunt est quis ultrices. Fusce at posuere tortor, sit amet convallis lacus. Nullam id interdum diam. Vestibulum euismod turpis in turpis tempus, eu lobortis lectus commodo. Nunc id ligula tristique, elementum est nec, varius tortor. Vestibulum vitae aliquam sem. Vestibulum dictum lobortis placerat.
+
+Vivamus faucibus augue a tristique finibus. In sollicitudin massa sodales sollicitudin porta. Integer at feugiat est. Pellentesque sem mi, varius eget lorem ut, feugiat varius ligula. Morbi sit amet dapibus erat, ut laoreet eros. Nunc pulvinar sem a diam tempus sollicitudin. Curabitur pretium, ex a mattis accumsan, dolor felis accumsan metus, vitae lacinia nibh orci quis erat. Integer ullamcorper orci eu ante fermentum fermentum. Nulla dignissim ipsum in libero euismod, iaculis cursus ipsum interdum. Fusce purus elit, volutpat non risus at, egestas aliquet felis. Morbi elementum interdum orci, a sollicitudin purus consectetur nec. Suspendisse semper risus a nisi congue porta. Aenean sit amet lobortis velit. Maecenas ac tellus purus.
+''';
 
 class FlutterApp extends StatelessWidget {
   const FlutterApp({super.key});
@@ -22,18 +38,29 @@ class FlutterApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter Test'),
         ),
-        body: ListView(
-          children: [
-            for (final asset in okAssets)
-              SizedBox(
-                width: 300,
-                height: 300,
-                child: Lol(
-                  key: Key(asset),
-                  asset: asset,
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: ListView(
+            children: [
+              const Text(bigText),
+              for (final asset in okAssets)
+                SizedBox(
+                  width: 300,
+                  height: 300,
+                  child: Lol(
+                    key: Key(asset),
+                    asset: asset,
+                  ),
                 ),
+              Container(
+                decoration: ShaderDecoration(fragmentProgram: fp),
+                child: const Text(bigText),
               ),
-          ],
+              const Text(bigText),
+              const Text(bigText),
+              const Text(bigText),
+            ],
+          ),
         ),
       ),
     );
@@ -61,8 +88,9 @@ class Lol extends StatelessWidget {
           children: [
             Text('$asset worked fine'),
             Flexible(
-              child: ShaderTicker(shader: snap.data!.fragmentShader(),)
-            ),
+                child: ShaderTicker(
+              shader: snap.data!.fragmentShader(),
+            )),
           ],
         );
       },
